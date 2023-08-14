70c885e9fef9e9c806baec9d041aae2e049f1c36
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-08-13 23:01:24 -0500

    Testing local storage and string translation

--- cypress/e2e/speakItalianTest.cy.js
+++ cypress/e2e/speakItalianTest.cy.js
@@ -1,5 +1,5 @@
-describe("e2e test of Speak Italian in Austin website", () => {
-  it("navigates to the website", () => {
+describe("e2e tests of Speak Italian in Austin website", () => {
+  it("navigates to the website and asserts that language is changed when users click", () => {
     cy.visit("http://localhost:3000/");
 
     cy.get(".title")
@@ -8,5 +8,18 @@ describe("e2e test of Speak Italian in Austin website", () => {
 
     cy.get(".fa-language").should("have.text", " Languages").click();
     cy.get(".dropdown-menu").should("have.class", "show");
+    cy.get("#englishLink").should("be.visible");
+    cy.get("#italianLink").should("be.visible").click();
+    cy.get(".dropdown-menu").should("have.not.class", "show");
+
+    cy.getAllLocalStorage().then((data) => {
+      console.log(data);
+      expect(data).to.deep.equal({
+        "http://localhost:3000": {
+          siteLang: "it",
+        },
+      });
+    });
+    cy.get(".fa-language").should("have.text", " Lingue");
   });
 });
